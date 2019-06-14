f3cf78fdb9ca8dc25f033b8e52da6a60def906cb
Eric.Montelibano@bannerhealth.com <eric.montelibano@bannerhealth.com>
2019-06-14 15:04:56 -0400

    update

---
+++ .sfdx/typings/lwc/apex/recentAccounts.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/recentAccounts.recAcc" {
+  export default function recAcc(): Promise<any>;
+}
---
+++ force-app/main/default/lwc/recentAccounts/recentAccounts.js
@@ -0,0 +1,59 @@
+import {
+    LightningElement,
+    track,
+    wire
+} from 'lwc';
+
+import wordcloud from '@salesforce/resourceUrl/wordcloud2';
+import {
+    loadScript
+} from 'lightning/platformResourceLoader';
+import {
+    ShowToastEvent
+} from 'lightning/platformShowToastEvent';
+
+import getRecentAcc from '@salesforce/apex/recentAccounts.recAcc';
+
+export default class RecentAccounts extends LightningElement {
+    @track error;
+
+    @wire(getRecentAcc) recAcc;
+    cloudInit = false;
+
+    list = [
+        ['foo', 1],
+        ['bar', 5]
+    ];
+
+    renderedCallback() {
+        //console.log('FIRST DIV' +this.template.querySelector('div'));
+        //console.log('my canvas' + this.template.querySelector('canvas.my_canvas')); 
+        if (this.cloudInit) {
+            return;
+        }
+        this.cloudInit = true;
+      Promise.all([
+                loadScript(this, wordcloud)
+            ])
+            .then(() => {
+                this.initializeWordCloud();
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error loading ERROR',
+                        message: error.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+    }
+
+    initializeWordCloud(){
+        //console.log('>>> my list' + this.list);
+        WordCloud(this.template.querySelector('canvas.my_canvas'), {
+            list: this.list 
+        });
+    } 
+    
+}
