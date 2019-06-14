807c4405c9f6dab28ed201c9fbfab24ae02ebad6
Eric.Montelibano@bannerhealth.com <eric.montelibano@bannerhealth.com>
2019-06-14 16:45:50 -0400

    pulled all objects and code from scratch org

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/wordcloud" {
+    var wordcloud: string;
+    export default wordcloud;
+}
+declare module "@salesforce/resourceUrl/wordcloud2" {
+    var wordcloud2: string;
+    export default wordcloud2;
+}
