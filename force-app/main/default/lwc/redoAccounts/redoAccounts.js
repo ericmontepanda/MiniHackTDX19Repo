8a4bad1dc76e1763e71a2410bc08e4a244d760c7
Eric.Montelibano@bannerhealth.com <eric.montelibano@bannerhealth.com>
2019-06-16 20:15:54 -0400

    wordCloud Complete

--- force-app/main/default/lwc/redoAccounts/redoAccounts.js
+++ force-app/main/default/lwc/redoAccounts/redoAccounts.js
@@ -4,12 +4,37 @@ import {loadScript} from 'lightning/platformResourceLoader';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import getRecentAcc from '@salesforce/apex/recentAccounts.recAcc';
 import getRecentWrap from '@salesforce/apex/recentAccounts.recentlyViewed';
-export default class RedoAccounts extends LightningElement {
+import { NavigationMixin } from 'lightning/navigation';
+export default class RedoAccounts extends NavigationMixin(LightningElement) {
     @track error;
     @track accts;
     @wire(getRecentAcc) recAccounts;
-    @wire(getRecentWrap) recWrap;
     @track result;
+
+    @wire(getRecentWrap)
+    recWrap({
+        error,
+        data
+    }) {
+        this.accts = [];
+        console.log('wired data' + JSON.stringify(data));
+        if (data) {
+            data.forEach(acc => {
+                this.accts.push([
+                    acc.Name,
+                    acc.pos,
+                    acc.Id
+                ]);
+            });
+
+            this.initializeWordCloud();
+        } else if (error) {
+            this.error = error;
+            this.accts = [];
+        }
+        console.log('wired acccount ' + this.accts);
+        
+    }
    
     list = [
         ['foo', 12],
@@ -25,10 +50,10 @@ export default class RedoAccounts extends LightningElement {
         this.cloudInit = true;
         
         loadScript(this, wordcloud)
-            .then(() => {
+            /*.then(() => {
                 console.log('entering... scripts1');
                 this.initializeWordCloud();
-            })
+            })*/
 
             .catch(error => {
                 this.dispatchEvent(
@@ -46,27 +71,45 @@ export default class RedoAccounts extends LightningElement {
         console.log('entering word cloud');
         var mycanvas = this.template.querySelector("canvas.my_canvas");
 
-        getRecentWrap()
-             .then(result => {
-                 console.log('SUCCESS what is my results ' + this.recAccounts);
-                 //this.accts = result.data;
-             })
-             .catch(error => {
-                 console.log('ERROR what is my results ');
-                 this.error = error;
-             });
-
         console.log('starting canvas result:' +this.accts);
         
         window.WordCloud(mycanvas, {
-            list: this.list,
+            list: this.accts,
             minFontSize: 200,
             fontFamily: 'Times, serif',
             rotateRatio: 0.5,
             rotationSteps: 2,
             backgroundColor: '#FF007F',
+            click: function (item) {
+                alert(item[0] + ': ' + item[2]);
+                /*this.accountHomePageRef = {
+                    type: "standard__objectPage",
+                    attributes: {
+                        "objectApiName": "Account",
+                        "actionName": "home"
+                    }
+                };
+                this[NavigationMixin.Navigate](this.accountHomePageRef);*/
+                
 
+                window.location.href = '/lightning/r/Account/' + item[2]+'/view';
+                
+            }
         });
         console.log('ending word cloud');
     }
+
+    navigateToRecordViewPage() {
+        // View a custom object record.
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: '0015400000HYqhPAAT',
+                objectApiName: 'Account', // objectApiName is optional
+                actionName: 'view'
+            }
+        });
+    }
+    
+
 }
